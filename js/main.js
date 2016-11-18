7490cef6dcc7f1da7b2bea5fc4738beefddd03f7
sharmad247 <sharmad247@gmail.com>
2016-11-19 02:36:50 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -234,17 +234,30 @@
 		      	sLoader.fadeIn(); 
 
 		      },
+		      success: function(msg) {
 
-	            if (1) {
+	            // Message was sent
+	            if (true) {
 	            	sLoader.fadeOut(); 
 	               $('#message-warning').hide();
 	               $('#contactForm').fadeOut();
 	               $('#message-success').fadeIn();   
 	            }
-
+	            // There was an error
+	            else {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').html(msg);
+		            $('#message-warning').fadeIn();
+	            }
 
 		      },
+		      error: function() {
 
+		      	sLoader.fadeOut(); 
+		      	$('#message-warning').html("Something went wrong. Please try again.");
+		         $('#message-warning').fadeIn();
+
+		      }
 
 	      });     		
   		}
